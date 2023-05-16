e8a38e013c6332efa6a9a855ed21afcccc564928
michelle <mlu415@aucklanduni.ac.nz>
2023-05-17 00:48:32 +1200

    update loginpage

--- src/components/LoginPage/loginpage.tsx
+++ src/components/LoginPage/loginpage.tsx
@@ -1,49 +1,73 @@
-import { useState } from "react";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { Button, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth, googleProvider } from "../../firebase/firebase";
+import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 
-const Login: React.FC = () => {
+const LoginPage = () => {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
 
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-
-  const handleLogin = async () => {
-    setError("");
-
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
     try {
-      const auth = getAuth();
       await signInWithEmailAndPassword(auth, email, password);
+      navigate("/home");
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+
+  const handleGoogleSignin = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+      navigate("/home");
     } catch (error: any) {
       alert(error.message);
     }
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      {error && <p>{error}</p>}
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={handleEmailChange}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={handlePasswordChange}
-      />
-      <button onClick={handleLogin}>Login</button>
+    <div className="loginContainer">
+      <div className="loginContainerv2">
+        <h1>Welcome Back</h1>
+        <form onSubmit={handleLogin}>
+          <div className="inputContainer">
+            <Typography>EMAIL</Typography>
+            <TextField
+              className="textfield"
+              placeholder="Enter your email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="inputContainer">
+            <Typography>PASSWORD</Typography>
+            <TextField
+              className="textfield"
+              placeholder="Enter your password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <button type="submit" className="loginBTN">
+            LOGIN
+          </button>
+        </form>
+
+        <span className="or">or</span>
+
+        <button onClick={handleGoogleSignin} className="googleBTN">
+          <i className="fa-brands fa-google"></i> Sign in with Google
+        </button>
+      </div>
     </div>
   );
 };
 
-export default Login;
+export default LoginPage;
