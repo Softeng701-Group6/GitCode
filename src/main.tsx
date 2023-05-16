dce3823d330f28dd794082a0609aea3f93e97467
michelle <mlu415@aucklanduni.ac.nz>
2023-05-16 23:46:09 +1200

    setup default login and signup pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import LoginPage from "./components/Login/loginpage";
+import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <LoginPage />
-  </React.StrictMode>,
-  document.getElementById("root")
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
