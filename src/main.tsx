f24ff55e035df0acef58314e8c1ce8137eb65b32
michelle <mlu415@aucklanduni.ac.nz>
2023-05-16 23:35:18 +1200

    Update gitignore and setup base configs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import LoginPage from "./components/Login/loginpage";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LoginPage />
   </React.StrictMode>,
-)
+  document.getElementById("root")
+);
