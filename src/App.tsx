680e47aee67b26bf2fc809cd3d664e241a202b03
kwan528 <kwan772@aucklanduni.ac.nz>
2023-05-20 18:55:53 +1200

    git graph with react flow

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Graph from "./components/Graph";
+import GitGraph from "./components/GitGraph/GitGraph";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <div style={{ width: "100vw", height: "100vh", border: 'solid black 3px' }}>
-        <Graph />
+        <GitGraph />
     </div>
   );
 }
