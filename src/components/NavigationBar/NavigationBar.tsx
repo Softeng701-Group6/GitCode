3f63446dcb9231e792af192908f9aae00903efd9
mthe371 <79773147+mthe371@users.noreply.github.com>
2023-05-21 19:13:27 +1200

    Add navigation bar

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,14 +1,45 @@
+import { AppBar, Box, Button, IconButton, LinearProgress, Toolbar, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
 export default function NavigationBar() {
 
   const navigate = useNavigate();
+  var experience:number = 2;
 
   return (
-    <div>
-      <p>Navigation</p>
-      <button onClick={() => navigate('login')}>Login</button>
-      <button onClick={() => navigate('signup')}>Sign Up</button>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="absolute" style={{ background: '#252525' }}>
+        <Toolbar variant="regular">
+        <Typography variant="h5" component="a" href="/" sx={{ '&:hover': {
+    color: "yellow",
+  },font:'inter', flexGrow: 0, color: 'white', fontWeight: 'bold'}}>
+            Git
+          </Typography>
+          <Typography variant="h5" component="a" href="/" sx={{ '&:hover': {
+    color: "white",
+  },font:'inter', flexGrow: 0, color: 'yellow', fontWeight: 'bold'}}>
+            Code.
+          </Typography>
+          <Box sx={{m: 3}}/>
+          <Button onClick={() => navigate('login')} sx={{color:'white'}}>Login</Button>
+          <Button onClick={() => navigate('signup')} sx={{color:'white'}}>Sign Up</Button>
+          <Box sx={{ flexGrow: 1}}>
+          </Box>
+          <IconButton sx={{color: 'yellow'}}>
+            <AccountCircleOutlinedIcon/>
+          </IconButton>
+        </Toolbar>
+        <Box sx={{ width: '100%' }}>
+      <LinearProgress variant="determinate" value={experience} sx={{
+                  backgroundColor: '#343434',
+                  '& .MuiLinearProgress-bar': {
+                    backgroundColor: '#31884F'
+                  }
+                }} />
+    </Box>
+      </AppBar>
+    </Box>
+    
   );
 }
