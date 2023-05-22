0ec5d1878aef77937279da737d71ba0e2c007bec
retinfai <aferetipama@gmail.com>
2023-05-23 08:15:43 +1200

    feat: add parent component

--- src/components/GraphApplication/index.tsx
+++ src/components/GraphApplication/index.tsx
@@ -1,8 +1,33 @@
-
 import GitGraph from "../GitGraph";
 import Terminal from "../Terminal";
+import { Edge } from '../../models/types';
+
+import { ReactFlowProvider } from "reactflow";
+
+import { useState } from 'react';
+import { Box, Stack } from '@mui/material';
+
 
 const GraphApplication = () => {
 
-    return <h1>Graph Application</h1>
+    const [nodes, setNodes] = useState<string[]>([]);
+    const [edges, setEdges] = useState<Edge[]>([]);
+
+    const [remote, setRemote] = useState<string[]>([]);
+
+    const [HEAD, setHEAD] = useState<string>('');
+
+
+    return (
+        <Stack>
+            <Box>
+                <ReactFlowProvider>
+                    <GitGraph nodes={nodes} edges={edges} remote={remote} HEAD={HEAD} />
+                </ReactFlowProvider>
+            </Box>
+            <Box>
+                <Terminal />
+            </Box>
+        </Stack>
+    )
 }
