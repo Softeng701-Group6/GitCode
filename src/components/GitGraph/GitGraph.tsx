99f858316d8048df820a5acb0aaf18b84cb1bfae
retinfai <aferetipama@gmail.com>
2023-05-22 20:38:53 +1200

    refactor: move getLayout into file

--- src/components/GitGraph/GitGraph.tsx
+++ src/components/GitGraph/GitGraph.tsx
@@ -15,50 +15,12 @@ import "reactflow/dist/style.css";
 
 import CustomNode from "./CustomNode";
 import { initialEdges, initialNodes } from "./initial-nodes-edges.js";
-
-import dagre from 'dagre';
+import { getLayoutedElements } from "./Layout.ts";
 
 const nodeTypes = {
   custom: CustomNode
 };
 
-const dagreGraph = new dagre.graphlib.Graph();
-dagreGraph.setDefaultEdgeLabel(() => ({}));
-
-const nodeWidth = 172;
-const nodeHeight = 36;
-
-const getLayoutedElements = (nodes: Node[], edges: Edge[], direction = 'TB') => {
-  const isHorizontal = direction === 'LR';
-  dagreGraph.setGraph({ rankdir: direction });
-
-  nodes.forEach((node: Node) => {
-    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
-  });
-
-  edges.forEach((edge: Edge) => {
-    dagreGraph.setEdge(edge.source, edge.target);
-  });
-
-  dagre.layout(dagreGraph);
-
-  nodes.forEach((node: Node) => {
-    const nodeWithPosition = dagreGraph.node(node.id);
-    node.targetPosition = isHorizontal ? 'left' : 'top';
-    node.sourcePosition = isHorizontal ? 'right' : 'bottom';
-
-    // We are shifting the dagre node position (anchor=center center) to the top left
-    // so it matches the React Flow node anchor point (top left).
-    node.position = {
-      x: nodeWithPosition.x - nodeWidth / 2,
-      y: nodeWithPosition.y - nodeHeight / 2,
-    };
-
-    return node;
-  });
-
-  return { nodes, edges };
-};
 
 const GitGraph = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
