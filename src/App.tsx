927f660cdeebc27a1ac261cab26e52e996890b00
retinfai <aferetipama@gmail.com>
2023-05-22 19:42:12 +1200

    feat: dynamically layout nodes

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Graph from "./components/Graph";
+import { ReactFlowProvider } from "reactflow";
 import GitGraph from "./components/GitGraph/GitGraph";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <div style={{ width: "100vw", height: "100vh", border: 'solid black 3px' }}>
+      <ReactFlowProvider>
         <GitGraph />
+      </ReactFlowProvider>
     </div>
   );
 }
