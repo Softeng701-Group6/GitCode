3b1c26cd8c00717cfd3ea312d42faf9cfd28143a
michelle <mlu415@aucklanduni.ac.nz>
2023-05-23 10:26:00 +1200

    fix missing firebaseconfig import

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,11 +2,28 @@
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { initializeApp, FirebaseApp } from "firebase/app";
-import firebaseConfig from "./firebaseConfig";
+import firebaseConfigTemplate from './firebaseConfig.example';
 
-const app: FirebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const firestore = getFirestore(app);
-const googleProvider = new GoogleAuthProvider();
+let app: FirebaseApp;
+let auth;
+let firestore;
+let googleProvider;
+
+(async () => {
+  let firebaseConfig;
+
+  try {
+    const configModule = await import('./firebaseConfig');
+    firebaseConfig = configModule.default;
+  } catch (err) {
+    console.log("Using template config");
+    firebaseConfig = firebaseConfigTemplate;
+  }
+
+  app = initializeApp(firebaseConfig);
+  auth = getAuth(app);
+  firestore = getFirestore(app);
+  googleProvider = new GoogleAuthProvider();
+})();
 
 export { auth, firestore, googleProvider };
