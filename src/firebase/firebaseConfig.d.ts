036220cfe2d33b6055f7a5ee121975d3bdfcdd24
michelle <mlu415@aucklanduni.ac.nz>
2023-05-23 10:38:21 +1200

    try a declare module

---
+++ src/firebase/firebaseConfig.d.ts
@@ -0,0 +1,11 @@
+// firebaseConfig.d.ts
+declare const firebaseConfig: {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+};
+
+export default firebaseConfig;
