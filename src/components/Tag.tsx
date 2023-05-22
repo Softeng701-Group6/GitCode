ef1e4d6839b8b045f19de8d7030dc84aba1629a2
michelle <mlu415@aucklanduni.ac.nz>
2023-05-23 10:01:24 +1200

    resolve errors

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,27 +1,34 @@
-import { Box, Color, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 interface Props {
-  color: string
-  children: string
+  color: string;
+  children: string;
 }
 
-export default function Tag({color, children}: Props) {
+export default function Tag({ color, children }: Props) {
   return (
-    <Box sx={{
-      display: "flex",
-      paddingX: 2,
-      paddingY: 1,
-      marginX: 0.5,
-      height: "min-content",
-      borderRadius: 1,
-      background: color,
-      alignItems: "center"
-    }}>
-      <Typography variant="button" sx={{
-        color: "white",
-        fontWeight: "bold",
-        fontSize: 10
-      }}>{children}</Typography>
+    <Box
+      sx={{
+        display: "flex",
+        paddingX: 2,
+        paddingY: 1,
+        marginX: 0.5,
+        height: "min-content",
+        borderRadius: 1,
+        background: color,
+        alignItems: "center",
+      }}
+    >
+      <Typography
+        variant="button"
+        sx={{
+          color: "white",
+          fontWeight: "bold",
+          fontSize: 10,
+        }}
+      >
+        {children}
+      </Typography>
     </Box>
   );
 }
--- src/pages/LevelSelectPage/LevelCard.tsx
+++ src/pages/LevelSelectPage/LevelCard.tsx
@@ -1,24 +1,23 @@
-import { Box, Card, CardContent, Stack, Typography, styled } from "@mui/material";
-import StarBorderIcon from '@mui/icons-material/StarBorder';
-import StarIcon from '@mui/icons-material/Star';
+import { Box, Stack, Typography } from "@mui/material";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import StarIcon from "@mui/icons-material/Star";
 import { ReactElement, useState } from "react";
 import { yellow } from "@mui/material/colors";
 import Frame from "../../components/Frame";
 
 const starIconStyle = {
   fontSize: 30,
-  color: yellow[600]
-}
+  color: yellow[600],
+};
 
 interface Props {
-  level: string
-  difficulty: string
-  tags?: ReactElement[]
+  level: string;
+  difficulty: string;
+  tags?: ReactElement[];
 }
 
-export default function LevelCard({level, difficulty, tags}: Props) {
-
-  const [isCompleted, setCompleted] = useState(false);
+export default function LevelCard({ level, difficulty, tags }: Props) {
+  // const [isCompleted, setCompleted] = useState(false);
   const [starred, setStarred] = useState(false);
 
   function toggleStarred() {
@@ -26,37 +25,56 @@ export default function LevelCard({level, difficulty, tags}: Props) {
   }
 
   return (
-    <Frame sx={{
-      marginBottom: 2
-    }}>
-      <Stack direction="row" sx={{
-        display: "flex",
-        position: "relative",
-        alignItems: "center"
-      }}>
+    <Frame
+      sx={{
+        marginBottom: 2,
+      }}
+    >
+      <Stack
+        direction="row"
+        sx={{
+          display: "flex",
+          position: "relative",
+          alignItems: "center",
+        }}
+      >
         <Stack direction="column" spacing={2}>
-          <Typography variant="h4" sx={{
-            textAlign: "left"
-          }}>{level}</Typography>
+          <Typography
+            variant="h4"
+            sx={{
+              textAlign: "left",
+            }}
+          >
+            {level}
+          </Typography>
 
           <Stack direction="row" alignItems="center">
-            <Typography variant="overline" sx={{
-              paddingX: 2
-            }}>{difficulty}</Typography>
+            <Typography
+              variant="overline"
+              sx={{
+                paddingX: 2,
+              }}
+            >
+              {difficulty}
+            </Typography>
 
             {tags}
           </Stack>
         </Stack>
 
-        <Box onClick={toggleStarred} sx={{
-          position: "absolute",
-          right: 0
-        }}>{starred ? 
-          <StarBorderIcon sx={starIconStyle}/> :
-          <StarIcon sx={starIconStyle} />}
+        <Box
+          onClick={toggleStarred}
+          sx={{
+            position: "absolute",
+            right: 0,
+          }}
+        >
+          {starred ? (
+            <StarBorderIcon sx={starIconStyle} />
+          ) : (
+            <StarIcon sx={starIconStyle} />
+          )}
         </Box>
-        
-        
       </Stack>
     </Frame>
   );
