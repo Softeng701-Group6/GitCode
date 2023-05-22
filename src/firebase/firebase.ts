2aa652a282ad6957903b4ede227b6df8290717da
michelle <mlu415@aucklanduni.ac.nz>
2023-05-23 11:16:53 +1200

    try catch

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,9 +1,21 @@
+/* eslint-disable prefer-const */
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { initializeApp, FirebaseApp } from "firebase/app";
-import firebaseConfig from "./firebaseConfig";
+import { initializeApp } from "firebase/app";
 
-const app: FirebaseApp = initializeApp(firebaseConfig);
+let app;
+let firebaseConfig;
+
+try {
+  firebaseConfig = require("./firebaseConfig").default;
+} catch (error) {
+  console.error("Failed to load Firebase configuration:", error);
+  firebaseConfig = require("./firebaseConfig.example").default;
+}
+
+app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 const firestore = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
