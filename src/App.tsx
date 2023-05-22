6e062535b5403900d9bb1c947931a9985468aa5c
michelle <mlu415@aucklanduni.ac.nz>
2023-05-22 23:50:41 +1200

    Fix routing, move navbar import to within the pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
 import { Route, Routes } from "react-router-dom";
 import SignupPage from "./pages/SignUpPage/SignUpPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
-import HomePage from "./pages/HomePage/HomePage";
-import PageLayout from "./components/PageLayout";
 import { ThemeProvider } from "@mui/material";
 import { defaultTheme } from "./themes/defaultTheme";
 import LevelSelectPage from "./pages/LevelSelectPage/LevelSelectPage";
 import TestPage from "./components/TestPage.tsx";
 import LandingPage from "./components/LandingPage/landingpage";
 import "./App.css";
+import DiscussionPage from "./pages/DiscussionPage/DiscussionPage.tsx";
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Routes>
-        <Route path="/" element={<LandingPage />}>
-          <Route index element={<HomePage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="signup" element={<SignupPage />} />
+    <div>
+      <ThemeProvider theme={defaultTheme}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
           <Route path="/test" element={<TestPage />} />
           <Route path="/home" element={<LevelSelectPage />} />
-        </Route>
-      </Routes>
-    </ThemeProvider>
+          <Route path="/discussion" element={<DiscussionPage />} />
+        </Routes>
+      </ThemeProvider>
+    </div>
   );
 }
 
---
+++ src/pages/DiscussionPage/DiscussionPage.tsx
@@ -0,0 +1,9 @@
+import NavigationBar from "../../components/NavigationBar/NavigationBar";
+
+export default function DiscussionPage() {
+  return (
+    <div>
+      <NavigationBar />
+    </div>
+  );
+}
