91456af25f17ec0a19995dbc9481cfbb21d88037
mthe371 <79773147+mthe371@users.noreply.github.com>
2023-05-23 14:25:11 +1200

    Add level description inital component

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,17 +1,16 @@
+import React from "react";
 import { AppBar, Box, Button, IconButton, LinearProgress, Menu, MenuItem, Toolbar, Tooltip, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import React from "react";
+import './NavigationBar.css'; // Import the CSS file
 
 const menuSettings = ['Profile', 'Logout'];
 
 export default function NavigationBar() {
-
   const navigate = useNavigate();
-  var experience:number = 2;
-
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
+  var experience:number = 2;
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
   };
@@ -21,32 +20,27 @@ export default function NavigationBar() {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="absolute" style={{ background: '#252525' }}>
-        <Toolbar variant="regular">
-        <Typography variant="h5" component="a" href="/" sx={{ '&:hover': {
-    color: "#FFB800",
-  },font:'inter', flexGrow: 0, color: 'white', fontWeight: 'bold'}}>
+    <Box className="root">
+      <AppBar position="absolute" style={{ background: '#252525'}}>
+        <Toolbar className="toolbar">
+          <Typography variant="h6" component="a" href="/" className="title">
             Git
           </Typography>
-          <Typography variant="h5" component="a" href="/" sx={{ '&:hover': {
-    color: "white",
-  },font:'inter', flexGrow: 0, color: '#FFB800', fontWeight: 'bold'}}>
+          <Typography variant="h6" component="a" href="/" className="subTitle">
             Code.
           </Typography>
-          <Box sx={{m: 3}}/>
-          <Button onClick={() => navigate('login')} sx={{color:'white'}}>Login</Button>
-          <Button onClick={() => navigate('signup')} sx={{color:'white'}}>Sign Up</Button>
-          <Box sx={{ flexGrow: 1}}>
-          </Box>
+          <Box sx={{ m: 3 }} />
+          <Button onClick={() => navigate('login')} className="loginButton">Login</Button>
+          <Button onClick={() => navigate('signup')} className="signupButton">Sign Up</Button>
+          <Box sx={{ flexGrow: 1 }}></Box>
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open profile settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <AccountCircleOutlinedIcon sx={{size: 'large',color: '#FFB800'}}/>
+                <AccountCircleOutlinedIcon className="userIcon" />
               </IconButton>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px'}}
+              className="userMenu"
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
@@ -62,7 +56,7 @@ export default function NavigationBar() {
               onClose={handleCloseUserMenu}
             >
               {menuSettings.map((menuSetting) => (
-                <MenuItem key={menuSetting} onClick={handleCloseUserMenu}>
+                <MenuItem key={menuSetting} onClick={handleCloseUserMenu} className="menuItem">
                   <Typography textAlign="center">{menuSetting}</Typography>
                 </MenuItem>
               ))}
@@ -71,14 +65,13 @@ export default function NavigationBar() {
         </Toolbar>
         <Box sx={{ width: '100%' }}>
           <LinearProgress variant="determinate" value={experience} sx={{
-                      backgroundColor: '#343434',
-                      '& .MuiLinearProgress-bar': {
-                        backgroundColor: '#31884F'
-                      }
+              backgroundColor: '#343434',
+              '& .MuiLinearProgress-bar': {
+              backgroundColor: '#31884F'
+            }
           }} />
         </Box>
       </AppBar>
     </Box>
-    
   );
 }
