cadd97c0c210b8940f8efe42e75358677b079e34
Keitel Anana <88295540+kana140@users.noreply.github.com>
2023-05-23 13:05:49 +1200

    deleted signuppage.css file, modified loginpage and signuppage to use inline styling

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField, Typography, Container, Grid, Stack, Box } from "@mui/material";
+import { Button, TextField, Typography, Container, Grid, Box } from "@mui/material";
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { auth, googleProvider } from "../../firebase/firebase";
@@ -41,73 +41,73 @@ const LoginPage = () => {
       >
         
         <Typography variant="h2" 
-            sx={{ 
-              fontWeight: 'bold', 
-              color: 'white',
-              margin: '4%'
-            }}> Login 
-          </Typography>
-        
-          <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
-            <Typography sx={{ color: 'white', textAlign: 'left'}}>EMAIL</Typography>
-            <TextField
-              required
-              fullWidth
-              id="email"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              placeholder=""
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              size="small"
-              sx={{
-                mt: 1, mb: 2,
-                input: {color: 'white'},
-                border: '1px solid white',
-                borderRadius: '9px',
-              }}
-            />
-            <Typography sx={{ color: 'white', textAlign: 'left'}}>PASSWORD</Typography>
-            <TextField
-              required
-              fullWidth
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              placeholder=""
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              size="small"
-              sx={{
-                mt: 1, mb: 2,
-                input: {color: 'white'},
-                border: '1px solid white',
-                borderRadius: '9px'
-              }}
-            />
-            <Button
-              type="submit"
-              variant="contained"
-              sx={{ mt: 3, mb: 2, backgroundColor: 'transparent', border: '1px solid white', '&:hover': {
-              backgroundColor: "#FFB800"}, borderRadius: '9px' }}
-            >
-              LOG IN
-            </Button>
-            <Grid container direction="column">
-              <Grid item xs>
-                <Button onClick={handleGoogleSignin} className="googleBTN" sx={{borderRadius: '9px', color:'white'}}>
-                  <GoogleIcon sx={{color:'white'}} /> SIGN IN WITH GOOGLE
-                </Button>
-              </Grid>
-              <Grid item xs>
-                <Link to="/signup" style={{color:'#FFB800', marginTop: '1%'}}>
-                  {"Don't have an account?"} <b> <u>Sign Up </u></b>
-                </Link>
-              </Grid>
+          sx={{ 
+            fontWeight: 'bold', 
+            color: 'white',
+            margin: '4%'
+          }}> Login 
+        </Typography>
+
+        <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
+          <Typography sx={{ color: 'white', textAlign: 'left'}}>EMAIL</Typography>
+          <TextField
+            required
+            fullWidth
+            id="email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            placeholder="Enter your email..."
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            size="small"
+            sx={{
+              mt: 1, mb: 3,
+              input: {color: 'white'},
+              border: '1px solid white',
+              borderRadius: '9px',
+            }}
+          /> 
+          <Typography sx={{ color: 'white', textAlign: 'left'}}>PASSWORD</Typography>
+          <TextField
+            required
+            fullWidth
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            placeholder="Enter your password..."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            size="small"
+            sx={{
+              mt: 1, mb: 3,
+              input: {color: 'white'},
+              border: '1px solid white',
+              borderRadius: '9px'
+            }}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ mt: 3, mb: 2, backgroundColor: 'transparent', border: '1px solid white', '&:hover': {
+            backgroundColor: "#FFB800"}, borderRadius: '9px' }}
+          >
+            LOG IN
+          </Button>
+          <Grid container direction="column">
+            <Grid item xs>
+              <Button onClick={handleGoogleSignin} sx={{borderRadius: '9px', color:'white'}}>
+                <GoogleIcon sx={{color:'white'}} /> SIGN IN WITH GOOGLE
+              </Button>
             </Grid>
-          </Box>
+            <Grid item xs>
+              <Link to="/signup" style={{color:'#FFB800', marginTop: '1%'}}>
+                {"Don't have an account?"} <b> <u>Sign Up </u></b>
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
       </Box>
     </Container>
 
