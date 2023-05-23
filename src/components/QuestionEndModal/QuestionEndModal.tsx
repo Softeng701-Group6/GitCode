d89bca3f38b9ce80ccc4633b352cffc11d858d25
michelle <mlu415@aucklanduni.ac.nz>
2023-05-23 14:11:45 +1200

    update to use module.css

--- src/components/QuestionEndModal/QuestionEndModal.tsx
+++ src/components/QuestionEndModal/QuestionEndModal.tsx
@@ -1,7 +1,7 @@
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import "./QuestionEndModal.css";
+import styles from "./QuestionEndModal.module.css";
 import tick from "../../assets/green_check.png";
 import CloseIcon from "@mui/icons-material/Close";
 
@@ -21,36 +21,39 @@ export default function QuestionEndModal({ onClose }: { onClose: () => void }) {
 
   return (
     <Modal open={true} onClose={handleCloseModal}>
-      <Box className="modal-container">
-        <div className="modal-close-button" onClick={handleCloseModal}>
+      <Box className={styles["modal-container"]}>
+        <div
+          className={styles["modal-close-button"]}
+          onClick={handleCloseModal}
+        >
           <CloseIcon />
         </div>
-        <Typography variant="h4" className="modal-title">
+        <Typography variant="h4" className={styles["modal-title"]}>
           Level Complete!
         </Typography>
-        <Typography variant="h4" className="modal-question-title">
+        <Typography variant="h4" className={styles["modal-question-title"]}>
           1. Commit and Push
         </Typography>
-        <img className="green-tick" src={tick} />
-        <Typography variant="body1" className="modal-text">
+        <img className={styles["green-tick"]} src={tick} alt="Green Tick" />
+        <Typography variant="body1" className={styles["modal-text"]}>
           Take a moment to reflect on the Lesson.
         </Typography>
-        <Typography variant="body2" className="modal-text">
+        <Typography variant="body2" className={styles["modal-text"]}>
           Key Commands Used In this Lesson:
         </Typography>
-        <ul className="modal-key-commands">
+        <ul className={styles["modal-key-commands"]}>
           <li>git commit -m "Message"</li>
           <li>git push</li>
         </ul>
-        <Typography variant="body2" className="modal-text">
+        <Typography variant="body2" className={styles["modal-text"]}>
           By committing and pushing changes regularly, you maintain a reliable
           and traceable history of your project, enabling seamless collaboration
           with other developers.
         </Typography>
-        <Typography variant="body2" className="modal-text">
+        <Typography variant="body2" className={styles["modal-text"]}>
           For more information and to continue the discussion, please visit the
           discussion page for this question{" "}
-          <a href="/home" className="modal-link">
+          <a href="/home" className={styles["modal-link"]}>
             here
           </a>
           .
