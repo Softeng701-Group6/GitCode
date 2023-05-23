63e8d99c6726cf538593b03d20d6ec55335b7e03
kwan528 <kwan772@aucklanduni.ac.nz>
2023-05-23 20:37:14 +1200

    dynamically adding nodes

--- src/components/Terminal/index.tsx
+++ src/components/Terminal/index.tsx
@@ -1,14 +1,46 @@
-import { Edge } from "../../models/types";
+import ReactFlow, {
+    Node,
+    Edge
+  } from "reactflow";
+  import { useEffect } from "react";
 
 interface GraphSetter {
-    setNodes: (nodes: string[]) => void;
+    setNodes: (nodes: Node[]) => void;
     setEdges: (edges: Edge[]) => void;
     setRemote: (nodes: string[]) => void;
     setHEAD: (head: string) => void;
+    nodes: Node[];
+    edges: Edge[];
+    remote: String[];
+    HEAD: String;
 }
 
-export default function Terminal({setNodes, setEdges, setRemote, setHEAD} : GraphSetter) {
+export default function Terminal({setNodes, setEdges, setRemote, setHEAD, nodes, edges, remote, HEAD} : GraphSetter) {
 
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          const newNode: Node = {
+            id: `${nodes.length+1}`,
+            data: { label: `New Node ${nodes.length+1}` },
+            position: { x: 0, y: 0 }
+          };
+
+          const newEdge: Edge = { id: "e"+ (nodes.length) +"-"+ newNode.id, source: newNode.id, target: `${(nodes.length)}` }
+    
+          console.log(nodes);
+    
+          setNodes([...nodes, newNode]);
+          setEdges([...edges, newEdge]);
+        }, 3000);
+
+        
+    
+        return () => {
+          clearInterval(interval);
+        };
+      }, [setNodes, nodes]);
+    
 
     return <h1>Terminal Component</h1>
 }
