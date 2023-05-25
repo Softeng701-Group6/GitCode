6bd1d66e0cadb036037a5c85f6a40fd015f8c351
ochu761 <75078332+AoMedium@users.noreply.github.com>
2023-05-25 21:52:24 +1200

    basic question page styling

--- src/pages/QuestionPage/QuestionPage.tsx
+++ src/pages/QuestionPage/QuestionPage.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
 import QuestionEndModal from "../../components/QuestionEndModal/QuestionEndModal";
 import "./QuestionPage.module.css";
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import QuestionDescription from "./QuestionDescription";
 import styles from "./QuestionPage.module.css";
 
@@ -22,7 +22,11 @@ const QuestionPage = () => {
       <NavigationBar />
       <Grid className={styles["main-grid"]} container spacing={2} sx={{}}>
         <Grid item xs={4}>
-          <QuestionDescription onSubmit={handleModalOpen} />
+          <QuestionDescription onSubmit={handleModalOpen} title={"1. Commit and Push"}>
+            <Typography variant="body1">ellus. 2. Morbi id tortor id enim consectetur consectetur. Praesent orci erat, consectetur quis efficitur pretium, auctor blandit lorem. Donec lobortis arcu ac dui luctus tincidunt. 3. Proin scelerisque arcu sapien, eu tristique lorem dictum eget. Suspendisse mollis tempor ligula nec iaculis. Quisque maximus interdum nunc, ut convallis quam pretium vitae. Duis posuere neque urna, id auctor arcu consectetur id. Ut mollis lectus tortor, eu tempus quam vestibulum at. Integer in leo non nisi scelerisque molestie. Nullam ut volutpat ante. 4. Duis ultrices erat urna, eu semper augue euismod eget. Nu</Typography>
+            <Typography variant="body1">ellus. 2. Morbi id tortor id enim consectetur consectetur. Praesent orci erat, consectetur quis efficitur pretium, auctor blandit lorem. Donec lobortis arcu ac dui luctus tincidunt. 3. Proin scelerisque arcu sapien, eu tristique lorem dictum eget. Suspendisse mollis tempor ligula nec iaculis. Quisque maximus interdum nunc, ut convallis quam pretium vitae. Duis posuere neque urna, id auctor arcu consectetur id. Ut mollis lectus tortor, eu tempus quam vestibulum at. Integer in leo non nisi scelerisque molestie. Nullam ut volutpat ante. 4. Duis ultrices erat urna, eu semper augue euismod eget. Nu</Typography>
+            <Typography variant="body1">ellus. 2. Morbi id tortor id enim consectetur consectetur. Praesent orci erat, consectetur quis efficitur pretium, auctor blandit lorem. Donec lobortis arcu ac dui luctus tincidunt. 3. Proin scelerisque arcu sapien, eu tristique lorem dictum eget. Suspendisse mollis tempor ligula nec iaculis. Quisque maximus interdum nunc, ut convallis quam pretium vitae. Duis posuere neque urna, id auctor arcu consectetur id. Ut mollis lectus tortor, eu tempus quam vestibulum at. Integer in leo non nisi scelerisque molestie. Nullam ut volutpat ante. 4. Duis ultrices erat urna, eu semper augue euismod eget. Nu</Typography>
+          </QuestionDescription>
         </Grid>
         <Grid item xs={8}>
           {/* TODO: add Graph-Terminal parent component here */}
