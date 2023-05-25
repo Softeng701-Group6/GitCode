9847b0b286f898dc05b0a729fe2c28429116da0d
ochu761 <75078332+AoMedium@users.noreply.github.com>
2023-05-25 22:12:18 +1200

    fix: outdated LevelDescription import

--- src/pages/LevelSelectPage/LevelSelectPage.tsx
+++ src/pages/LevelSelectPage/LevelSelectPage.tsx
@@ -6,7 +6,7 @@ import { Grid, Stack } from "@mui/material";
 import Frame from "../../components/Frame";
 import styles from './LevelSelectPage.module.css'
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
-import LevelDescription from '../../components/LevelDescription';
+import LevelDescription from '../../components/LevelDescription/LevelDescription';
 import LevelDiscussion from "../../components/LevelDiscussion/LevelDiscussion.tsx";
 import { DUMMY_DATA_QUESTIONS } from "../../firebase/db/dummyData.ts";
 import { useEffect, useState } from "react";
