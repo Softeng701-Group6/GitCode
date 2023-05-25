50968de30ab9ac413fd9dcb4be618269192cc94c
ochu761 <75078332+AoMedium@users.noreply.github.com>
2023-05-25 22:42:11 +1200

    add graph-terminal component to question page + question content

--- src/pages/QuestionPage/QuestionPage.tsx
+++ src/pages/QuestionPage/QuestionPage.tsx
@@ -5,6 +5,7 @@ import "./QuestionPage.module.css";
 import { Grid, Typography } from "@mui/material";
 import QuestionDescription from "./QuestionDescription";
 import styles from "./QuestionPage.module.css";
+import GraphApplicationTest from "../GraphApplicationTest/GraphApplicationTest";
 
 const QuestionPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -22,14 +23,23 @@ const QuestionPage = () => {
       <NavigationBar />
       <Grid className={styles["main-grid"]} container spacing={2} sx={{}}>
         <Grid item xs={4}>
-          <QuestionDescription onSubmit={handleModalOpen} title={"1. Commit and Push"}>
-            <Typography variant="body1">ellus. 2. Morbi id tortor id enim consectetur consectetur. Praesent orci erat, consectetur quis efficitur pretium, auctor blandit lorem. Donec lobortis arcu ac dui luctus tincidunt. 3. Proin scelerisque arcu sapien, eu tristique lorem dictum eget. Suspendisse mollis tempor ligula nec iaculis. Quisque maximus interdum nunc, ut convallis quam pretium vitae. Duis posuere neque urna, id auctor arcu consectetur id. Ut mollis lectus tortor, eu tempus quam vestibulum at. Integer in leo non nisi scelerisque molestie. Nullam ut volutpat ante. 4. Duis ultrices erat urna, eu semper augue euismod eget. Nu</Typography>
-            <Typography variant="body1">ellus. 2. Morbi id tortor id enim consectetur consectetur. Praesent orci erat, consectetur quis efficitur pretium, auctor blandit lorem. Donec lobortis arcu ac dui luctus tincidunt. 3. Proin scelerisque arcu sapien, eu tristique lorem dictum eget. Suspendisse mollis tempor ligula nec iaculis. Quisque maximus interdum nunc, ut convallis quam pretium vitae. Duis posuere neque urna, id auctor arcu consectetur id. Ut mollis lectus tortor, eu tempus quam vestibulum at. Integer in leo non nisi scelerisque molestie. Nullam ut volutpat ante. 4. Duis ultrices erat urna, eu semper augue euismod eget. Nu</Typography>
-            <Typography variant="body1">ellus. 2. Morbi id tortor id enim consectetur consectetur. Praesent orci erat, consectetur quis efficitur pretium, auctor blandit lorem. Donec lobortis arcu ac dui luctus tincidunt. 3. Proin scelerisque arcu sapien, eu tristique lorem dictum eget. Suspendisse mollis tempor ligula nec iaculis. Quisque maximus interdum nunc, ut convallis quam pretium vitae. Duis posuere neque urna, id auctor arcu consectetur id. Ut mollis lectus tortor, eu tempus quam vestibulum at. Integer in leo non nisi scelerisque molestie. Nullam ut volutpat ante. 4. Duis ultrices erat urna, eu semper augue euismod eget. Nu</Typography>
+          <QuestionDescription onSubmit={handleModalOpen} title={"Lesson 1: Introduction to Git Commit and Push"}>
+            <Typography variant="body1">Welcome to your first lesson on Git, the widely used version control system in the software development industry. In this lesson, we will delve into the fundamental concepts of Git commit and push, which form the backbone of collaborative code management.</Typography>
+
+            <Typography variant="h3">Learning Outcome:</Typography>
+            <Typography variant="body1">By the end of this lesson, you will understand the importance of Git commit and push, and how they enable you to track and share changes in your codebase effectively.</Typography>
+
+            <Typography variant="h3">Activity: Committing and Pushing Changes</Typography>
+            <ul>
+              <li>In this activity, we will guide you through the process of committing and pushing changes to a Git repository.</li>
+              <li>All changes have already been added and staged, now we need to commit and push our changes onto main.</li>
+              <li>Use git commit commands in the terminal to complete this task and click submit to check!</li>
+              <li>Feel free to use hints located on the right side of the screen if you are require further guidance.</li>
+            </ul>
           </QuestionDescription>
         </Grid>
         <Grid item xs={8}>
-          {/* TODO: add Graph-Terminal parent component here */}
+          <GraphApplicationTest />
         </Grid>
       </Grid>
       {isModalOpen && <QuestionEndModal onClose={handleModalClose} />}
