de9f8e32ee6aaac6c519c22aea822497c662a12f
retinfai <aferetipama@gmail.com>
2023-05-28 09:06:13 +1200

    feat: redirect authenticated users to login

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import SignupPage from "./pages/SignUpPage/SignUpPage";
-import LoginPage from "./pages/LoginPage/LoginPage";
 import { ThemeProvider } from "@mui/material";
 import { defaultTheme } from "./themes/defaultTheme";
+import SignupPage from "./pages/SignUpPage/SignUpPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
 import LevelSelectPage from "./pages/LevelSelectPage/LevelSelectPage";
 import TestPage from "./components/TestPage.tsx";
 import LandingPage from "./pages/LandingPage/landingpage";
-import GraphApplicationTest from "./pages/GraphApplicationTest/GraphApplicationTest.tsx";
-import "./App.css";
 import QuestionPage from "./pages/QuestionPage/QuestionPage.tsx";
+import ProtectedRoute from "./components/ProtectedRoute/index.tsx";
+import { useState } from "react";
+import { auth } from "./firebase/firebase.ts";
+import { User as FirebaseUser } from "firebase/auth";
 
 function App() {
+  const [user, setUser] = useState<FirebaseUser | null>(null);
+  const [isLoading, setLoading] = useState(true);
+
+  auth.onAuthStateChanged((firebaseUser) => {
+    setUser(firebaseUser);
+    setLoading(false);
+  });
+
+  if (isLoading) return <h1>Loading...</h1>;
+
   return (
     <div>
       <ThemeProvider theme={defaultTheme}>
@@ -19,9 +32,22 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/test" element={<TestPage />} />
-          <Route path="/home" element={<LevelSelectPage />} />
-          <Route path="/graph" element={<GraphApplicationTest />} />
-          <Route path="/question" element={<QuestionPage />} />
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute user={user}>
+                <LevelSelectPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/question"
+            element={
+              <ProtectedRoute user={user}>
+                <QuestionPage />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </ThemeProvider>
     </div>
