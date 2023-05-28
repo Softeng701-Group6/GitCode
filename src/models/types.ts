189fd88177120d9378ce76cd4f3eb3185f5d9475
Nick <thenickys123@gmail.com>
2023-05-29 01:16:56 +1200

    Add all necessary fields

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,64 +1,65 @@
 import { DocumentReference } from "firebase/firestore";
 
 export interface GeneralObject {
-    id?: string;
+  id?: string;
 }
 
 export interface User extends GeneralObject {
-    name: string;
-    password: string;
-    expProgress: string;
-    level: string;
-    profileImg: string;
-    completedQuestions: DocumentReference[];  // Reference to Question
+  name: string;
+  expProgress: string;
+  level: string;
+  profileImg: string;
+  completedQuestions: string[];  // Reference to Question
+  attemptedQuestions: string[];  // Reference to Question
 }
 
 interface Description {
-    description: string;
-    activityArray: string[];
+  description: string;
+  activityArray: string[];
 }
 
 interface learningObjective {
-    objective: string;
-    outcomeArray: string[];
+  objective: string;
+  outcomeArray: string[];
 }
 
 interface Discussion {
-    statement: string;
-    commands: string[];
-    answers: Answer[];
+  statement: string;
+  commands: string[];
+  answers: Answer[];
 }
 
 interface Answer {
-    step: string;
-    explanation: string[];
+  step: string;
+  explanation: string[];
 }
 
 export interface Comment extends GeneralObject {
-    userId: DocumentReference;  // Reference to User
-    message: string;
-    upVotes: DocumentReference[];  // Reference to User
+  userId: DocumentReference;  // Reference to User
+  message: string;
+  upVotes: DocumentReference[];  // Reference to User
 }
 
 export interface Question extends GeneralObject {
-    title: string;
-    hints: string[];
-    summary: string[];
-    description: Description;
-    learningObjective: learningObjective;
-    discussion: Discussion;
-    commentIds: DocumentReference[];  // Reference to Comment
-    initialGraph: DocumentReference | null; // Reference to Graph
-    goalGraph: DocumentReference | null; // Reference to Graph
+  title: string;
+  hints: string[];
+  summary: string[];
+  description: Description;
+  learningObjective: learningObjective;
+  discussion: Discussion;
+  commentIds: DocumentReference[];  // Reference to Comment
+  initialGraph: DocumentReference | null; // Reference to Graph
+  goalGraph: DocumentReference | null; // Reference to Graph
 }
 
 export interface Edge {
-    source: string;
-    target: string;
+  source: string;
+  target: string;
 }
 
 export interface Graph extends GeneralObject {
-    name: string;
-    nodes: string[];
-    edges: Edge[];
+  name: string;
+  nodes: string[];
+  edges: Edge[];
+  headNode: string | null;
 }
