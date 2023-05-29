e8e1abcaf2464defa48caf9a36871a3ccfab6b51
ochu761 <75078332+AoMedium@users.noreply.github.com>
2023-05-29 21:09:20 +1200

    add dummy 2nd level

--- src/data/dummyData.ts
+++ src/data/dummyData.ts
@@ -4,7 +4,7 @@ import { Graph, Question } from "../models/types.ts";
 export const DUMMY_DATA_QUESTIONS: Question[] = [
   {
     id: "Commit and Push",
-    title: "Commit and Push",
+    title: "1. Commit and Push",
     hints: [
       "Once your changes are staged, it's time to commit them. Use the git commit command along with a descriptive commit message to document the purpose and nature of your changes. A well-written commit message is essential for clear communication within a development team.",
       "After committing your changes locally, it's time to share them with others by pushing the commits to a remote repository. Use the git push command to send your changes to the designated remote repository, allowing others to access and integrate your work.",
@@ -63,8 +63,8 @@ export const DUMMY_DATA_QUESTIONS: Question[] = [
     tags: [Tags.commit, Tags.push],
   },
   {
-    id: "Commit and TEshtalhfal",
-    title: "Commit and TEshtalhfal",
+    id: "Checkout and Branching",
+    title: "2. Checkout and Branching",
     hints: [
       "Once your changes are staged, it's time to commit them. Use the git commit command along with a descriptive commit message to document the purpose and nature of your changes. A well-written commit message is essential for clear communication within a development team.",
       "After committing your changes locally, it's time to share them with others by pushing the commits to a remote repository. Use the git push command to send your changes to the designated remote repository, allowing others to access and integrate your work.",
@@ -86,9 +86,9 @@ export const DUMMY_DATA_QUESTIONS: Question[] = [
     learningObjective: {
       objective: "By the end of this lesson, you will understand the importance of Git commit and push, and how they enable you to track and share changes in your codebase effectively.",
       outcomeArray: [
-        "Familiarity with Git Terminology: Students will become reacquainted with essential Git terminology, including \"commit,\" and \"push.\"",
-        "Committing Changes: Students will revisit how to create a commit in Git.",
-        "Pushing Commits: Students will grasp the concept of pushing commits to a remote repository, gaining knowledge about the purpose of remotes and the git push command."
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis posuere leo vitae nisi faucibus, vehicula suscipit ex posuere. Ut malesuada aliquam erat.",
+        "Sed porttitor, enim vitae molestie congue, lectus ex volutpat turpis, tincidunt ultrices velit lorem sed quam.",
+        "Cras nec velit sed urna rhoncus vehicula a eget lacus. Curabitur et tincidunt est. Maecenas non nunc nisl. Proin ac porta risus, non tincidunt sapien."
       ]
     },
     discussion: {
@@ -99,19 +99,17 @@ export const DUMMY_DATA_QUESTIONS: Question[] = [
       ],
       answers: [
         {
-          step: "Step 1: Commit the Changes",
+          step: "Step 1: Test",
           explanation: [
-            "Committing changes in Git is like taking a snapshot of your code at a specific point in time. It allows you to document the changes made and provide a clear description of the modifications",
-            "The -m flag stands for message and is followed by the commit message enclosed in quotes. The commit message is a brief description that explains what changes were made in the commit. It is important to provide a meaningful and descriptive commit message to help you and others understand the purpose of the commit.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis posuere leo vitae nisi faucibus, vehicula suscipit ex posuere. Ut malesuada aliquam erat. Sed porttitor, enim vitae molestie congue, lectus ex volutpat turpis, tincidunt ultrices velit lorem sed quam. Cras nec velit sed urna rhoncus vehicula a eget lacus. Curabitur et tincidunt est. Maecenas non nunc nisl. Proin ac porta risus, non tincidunt sapien. Mauris vitae quam erat. Nam in efficitur risus. Cras sodales scelerisque felis eget rhoncus."
           ]
         },
         {
-          step: "Step 2: Push the Commits",
+          step: "Step 2: Testing",
           explanation: [
-            "Once you have committed your changes, it's time to push them to a remote repository. Pushing commits means sending your committed changes to a shared repository that can be accessed by others. Use the command git push origin main to push your commits.",
-            "In this command, origin refers to the remote repository's name. You can replace it with the appropriate remote repository name if it differs. main represents the branch you are pushing to, which is typically the main branch in many Git workflows. However, if your repository uses a different main branch name (e.g., master), make sure to replace main with the correct branch name.",
-            "By executing git push origin main, you send your commits to the remote repository, making them available to others who have access to the repository. This allows for collaboration and synchronization of code changes among team members.",
-            "Remember, the git commit -m command and the git push origin main command are essential for documenting and sharing your code changes. By providing meaningful commit messages and regularly pushing your commits to the remote repository, you can effectively track and collaborate on your codebase."
+            "Aenean maximus non nunc ac interdum. Cras in dictum orci, sed ultricies ex. In suscipit elementum sem quis efficitur. Praesent cursus varius lorem sed finibus.",
+            "Praesent laoreet sapien dolor, ut egestas neque dapibus et. Maecenas pretium sit amet ligula at accumsan. Nullam ultrices vel nisl laoreet luctus.",
+            "Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur vitae auctor purus. Curabitur neque urna, ultrices eget massa eget, semper cursus est. Morbi ac mauris augue."
           ]
         }
       ]
@@ -119,7 +117,7 @@ export const DUMMY_DATA_QUESTIONS: Question[] = [
     // commentIds: [],
     initialGraph: null,
     goalGraph: null,
-    difficulty: Difficulty.EASY,
+    difficulty: Difficulty.MEDIUM,
     tags: [Tags.commit, Tags.push],
   }
 ]
