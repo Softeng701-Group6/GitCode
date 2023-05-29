9e9020c691ced44ee049002d19b5e77540c9e89f
poli648 <poli648@aucklanduni.ac.nz>
2023-05-29 23:13:33 +1200

    integrated scaffolding with scaffolding toggle

--- src/HelpBar/DirectedModeSwitch.tsx
+++ src/HelpBar/DirectedModeSwitch.tsx
@@ -1,17 +1,21 @@
 import { Switch, SwitchProps, Tooltip, styled, switchClasses } from "@mui/material";
 import { useState } from "react";
 
-export default function DirectedModeSwitch() {
+interface helpBarProp {
+    onToggle: (checked:boolean) => void
+  }
+
+export default function DirectedModeSwitch({onToggle}: helpBarProp) {
     const [checked, setChecked] = useState(false);
 
 
     const handleSwitch = (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(event.target.checked);
         setChecked(event.target.checked);
+        onToggle(event.target.checked);
     }
 
     return(
-        <Tooltip title= {checked ? "Restricted mode" : "Free mode"}>
+        <Tooltip title= {checked ? "Scaffolded mode" : "Free mode"}>
             <Switch sx={{
                 borderRadius: 2,
                 "& .MuiSwitch-switchBase.Mui-checked": {
--- src/components/GraphApplication/index.tsx
+++ src/components/GraphApplication/index.tsx
@@ -11,13 +11,11 @@ import HelpBar from "../../HelpBar/HelpBar";
 export default function GraphApplication({
   initialGraph,
   goalGraph,
-  isScaffolded,
   answers,
   setComplete,
 }: {
   initialGraph: Graph;
   goalGraph: Graph;
-  isScaffolded: boolean; 
   answers: string[];
   setComplete: (complete: boolean) => void;
 }) {
@@ -28,7 +26,7 @@ export default function GraphApplication({
   const [edges, setEdges] = useState<Edge[]>(initialEdges);
   const [remote, setRemote] = useState<Set<string>>(new Set(nodes));
   const [HEAD, setHEAD] = useState<string>(initialNodes[initialNodes.length - 1]);
-  const scaffolding = isScaffolded;
+  const [scaffolding, setScaffolding] = useState(false);
 
 
   const [branch, setBranch] = useState<string>("main");
@@ -40,6 +38,10 @@ export default function GraphApplication({
       ['main', ['1']]
     ])
   );
+
+  const onToggle = (checked: boolean) => {
+    setScaffolding(checked);
+  }
   
   useEffect(() => {
     if (nodes.length === goalNodes.length && edges.length === goalEdges.length && nodes.every((value, index) => value === goalNodes[index]) && JSON.stringify(edges) === JSON.stringify(goalEdges)) {
@@ -55,7 +57,7 @@ export default function GraphApplication({
           <GitGraph nodes={nodes} edges={edges} remote={remote} HEAD={HEAD} branch={branch}/>
         </ReactFlowProvider>
       </Box>
-      <HelpBar/>
+      <HelpBar onToggle={onToggle}/>
       <Box sx={{ height: "50%" }}>
         <Terminal
           setNodes={setNodes}
