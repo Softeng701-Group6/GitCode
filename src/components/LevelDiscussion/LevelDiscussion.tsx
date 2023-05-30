c028a27b3e66b3d4aa3d07d825fbb6d4448be054
Keitel Anana <88295540+kana140@users.noreply.github.com>
2023-05-30 19:32:46 +1200

    Updated format for LevelDiscussion.tsx

--- src/components/LevelDiscussion/LevelDiscussion.tsx
+++ src/components/LevelDiscussion/LevelDiscussion.tsx
@@ -32,9 +32,10 @@ export default function LevelDiscussion() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [allUsers, setAllUsers] = useState<User[]>([]);
 
+  isLevelAttempted();
+
   // Refresh the comments to get the latest
   async function init() {
-    isLevelAttempted();
     // Linking to firebase
     setAllUsers(await getCollection<User>(Collection.USERS));
 
@@ -101,110 +102,75 @@ export default function LevelDiscussion() {
       >
         Model Answer
       </Typography>
-      { levelAttempted ? 
-      <Stack>
+      {levelAttempted ? (
         <Stack>
-          <Typography sx={{ textAlign: "left", py: 2 }}>
-            {selectedQuestion.discussion.statement}
-          </Typography>
-          <Stack
-            direction="column"
-            spacing={2}
-            sx={{
-              my: 4,
-              borderRadius: 1,
-              alignItems: "flex-start",
-              marginRight: 4,
-              width: 0.92,
-              px: 4,
-              py: 4,
-              backgroundColor: "#1E1E1E",
-            }}
-          >
-            {selectedQuestion.discussion.commands.map((cmd) => (
-              <Typography sx={{ textAlign: "left" }} key={cmd}>
-                {cmd}
-              </Typography>
-            ))}
-          </Stack>
-          {selectedQuestion.discussion.answers.map((ans) => (
-            <Stack key={ans.step} sx={{ py: 2 }}>
-              <Typography sx={{ textAlign: "left", py: 2 }}>
-                {ans.step}
-              </Typography>
-              <ul>
-                {ans.explanation.map((line, index) => (
-                  <li key={index}>
-                    <Typography sx={{ textAlign: "left" }}>{line}</Typography>
-                  </li>
-                ))}
-              </ul>
-            </Stack>
-          ))}
-
-          {/* Footer */}
-          <Typography sx={{ textAlign: "left", py: 2 }}>
-            For more information, Checkout Atlassian's Page{" "}
-            <a href="https://www.atlassian.com/git/tutorials/learn-git-with-bitbucket-cloud">
-              here
-            </a>
-          </Typography>
-          <Typography variant="h3" sx={{ textAlign: "left", py: 2 }}>
-            Post your answers below, Is there another way to get the solution?
-          </Typography>
-        </Stack>
-
-      <Divider
-        className={styles["divider"]}
-        sx={{ marginTop: 4 }}
-        variant="middle"
-      />
-      <Box sx={{ flexGrow: 1 }}></Box>
-
-      {isLoading ? (
-        <Typography>Loading...</Typography>
-      ) : (
-        <Stack alignItems="flex-start" className={styles["comment-section"]}>
-          <Typography className={styles["comment-title"]} sx={{ width: 1 }}>
-            Comments
-          </Typography>
-
-          <Stack
-            className={styles["comment-sender"]}
-            direction="row"
-            sx={{ width: 1 }}
-          >
-            <TextField
-              className={styles["comment-box"]}
-              multiline
-              sx={{ width: 0.95 }}
-              rows={4}
-              value={commentToSend}
-              InputProps={{
-                style: { color: "white" },
-              }}
-              onChange={(event) => setCommentToSend(event.target.value)}
-              onKeyDown={(event) =>
-                event.key === "Enter" ? handleSendComment() : null
-              }
-            />
-            <Button
-              variant="outlined"
-              className={styles["comment-sender-button"]}
-              onClick={handleSendComment}
-            >
-              Send
-            </Button>
-          </Stack>
-
-          {comments.map((comment) => (
+          <Stack>
+            <Typography sx={{ textAlign: "left", py: 2 }}>
+              {selectedQuestion.discussion.statement}
+            </Typography>
             <Stack
-              key={comment.id}
-              className={styles["comment-read"]}
-              sx={{ width: 1 }}
+              direction="column"
+              spacing={2}
+              sx={{
+                my: 4,
+                borderRadius: 1,
+                alignItems: "flex-start",
+                marginRight: 4,
+                width: 0.92,
+                px: 4,
+                py: 4,
+                backgroundColor: "#1E1E1E",
+              }}
             >
-              <Typography sx={{ textAlign: "left", py: 2, px: 2 }}>
-                {allUsers.find((u: User) => u.id === comment.userId)!.email}
+              {selectedQuestion.discussion.commands.map((cmd) => (
+                <Typography sx={{ textAlign: "left" }} key={cmd}>
+                  {cmd}
+                </Typography>
+              ))}
+            </Stack>
+            {selectedQuestion.discussion.answers.map((ans) => (
+              <Stack key={ans.step} sx={{ py: 2 }}>
+                <Typography sx={{ textAlign: "left", py: 2 }}>
+                  {ans.step}
+                </Typography>
+                <ul>
+                  {ans.explanation.map((line, index) => (
+                    <li key={index}>
+                      <Typography sx={{ textAlign: "left" }}>{line}</Typography>
+                    </li>
+                  ))}
+                </ul>
+              </Stack>
+            ))}
+
+            {/* Footer */}
+            <Typography sx={{ textAlign: "left", py: 2 }}>
+              For more information, Checkout Atlassian's Page{" "}
+              <a href="https://www.atlassian.com/git/tutorials/learn-git-with-bitbucket-cloud">
+                here
+              </a>
+            </Typography>
+            <Typography variant="h3" sx={{ textAlign: "left", py: 2 }}>
+              Post your answers below, Is there another way to get the solution?
+            </Typography>
+          </Stack>
+
+          <Divider
+            className={styles["divider"]}
+            sx={{ marginTop: 4 }}
+            variant="middle"
+          />
+          <Box sx={{ flexGrow: 1 }}></Box>
+
+          {isLoading ? (
+            <Typography>Loading...</Typography>
+          ) : (
+            <Stack
+              alignItems="flex-start"
+              className={styles["comment-section"]}
+            >
+              <Typography className={styles["comment-title"]} sx={{ width: 1 }}>
+                Comments
               </Typography>
 
               <Stack
@@ -214,42 +180,82 @@ export default function LevelDiscussion() {
               >
                 <TextField
                   className={styles["comment-box"]}
-                  sx={{ width: 1, marginRight: 4 }}
                   multiline
+                  sx={{ width: 0.95 }}
                   rows={4}
-                  value={comment.message}
+                  value={commentToSend}
                   InputProps={{
-                    readOnly: true,
                     style: { color: "white" },
                   }}
+                  onChange={(event) => setCommentToSend(event.target.value)}
+                  onKeyDown={(event) =>
+                    event.key === "Enter" ? handleSendComment() : null
+                  }
                 />
-
-                {comment.upVotes.includes(user.uid) ? (
-                  <ThumbUpIcon
-                    className={styles["comment-vote"]}
-                    sx={{ pr: 4 }}
-                    onClick={() => {
-                      handleUpvote(comment);
-                    }}
-                  />
-                ) : (
-                  <ThumbUpAltOutlinedIcon
-                    className={styles["comment-vote"]}
-                    sx={{ pr: 4 }}
-                    onClick={() => {
-                      handleUpvote(comment);
-                    }}
-                  />
-                )}
-
-                <Typography>{comment.upVotes.length}</Typography>
+                <Button
+                  variant="outlined"
+                  className={styles["comment-sender-button"]}
+                  onClick={handleSendComment}
+                >
+                  Send
+                </Button>
               </Stack>
+
+              {comments.map((comment) => (
+                <Stack
+                  key={comment.id}
+                  className={styles["comment-read"]}
+                  sx={{ width: 1 }}
+                >
+                  <Typography sx={{ textAlign: "left", py: 2, px: 2 }}>
+                    {allUsers.find((u: User) => u.id === comment.userId)!.email}
+                  </Typography>
+
+                  <Stack
+                    className={styles["comment-sender"]}
+                    direction="row"
+                    sx={{ width: 1 }}
+                  >
+                    <TextField
+                      className={styles["comment-box"]}
+                      sx={{ width: 1, marginRight: 4 }}
+                      multiline
+                      rows={4}
+                      value={comment.message}
+                      InputProps={{
+                        readOnly: true,
+                        style: { color: "white" },
+                      }}
+                    />
+
+                    {comment.upVotes.includes(user.uid) ? (
+                      <ThumbUpIcon
+                        className={styles["comment-vote"]}
+                        sx={{ pr: 4 }}
+                        onClick={() => {
+                          handleUpvote(comment);
+                        }}
+                      />
+                    ) : (
+                      <ThumbUpAltOutlinedIcon
+                        className={styles["comment-vote"]}
+                        sx={{ pr: 4 }}
+                        onClick={() => {
+                          handleUpvote(comment);
+                        }}
+                      />
+                    )}
+
+                    <Typography>{comment.upVotes.length}</Typography>
+                  </Stack>
+                </Stack>
+              ))}
             </Stack>
-          ))}
+          )}
         </Stack>
+      ) : (
+        <HiddenDiscussion />
       )}
-      </Stack>
-      : <HiddenDiscussion />}
     </Stack>
   );
 }
