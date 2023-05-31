00000c6150e7e457343ea7187811af056f3cc868
michelle <mlu415@aucklanduni.ac.nz>
2023-05-31 15:16:55 +1200

    add incorrect modal

--- src/pages/QuestionPage/QuestionPage.tsx
+++ src/pages/QuestionPage/QuestionPage.tsx
@@ -1,10 +1,11 @@
 import { useContext, useState } from "react";
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
 import QuestionEndModal from "../../components/QuestionEndModal/QuestionEndModal";
-import { Grid, Typography} from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import QuestionDescription from "./QuestionDescription";
 import styles from "./QuestionPage.module.css";
 import GraphApplication from "../../components/GraphApplication";
+import GraphCheckModal from "../../components/GraphCheckModal/GraphCheckModal";
 
 import {
   gitCommitPushNodes,
@@ -15,11 +16,10 @@ import {
 import { LevelContext } from "../../context/LevelContext";
 
 const QuestionPage = () => {
-
   const { selectedQuestion } = useContext(LevelContext);
-
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isComplete, setComplete] = useState(false);
+  const [isGraphCheckModalOpen, setGraphCheckModalOpen] = useState(false);
 
   const handleModalOpen = () => {
     setIsModalOpen(true);
@@ -29,15 +29,26 @@ const QuestionPage = () => {
     setIsModalOpen(false);
   };
 
-  return(
+  const handleSubmit = () => {
+    if (isComplete) {
+      handleModalOpen();
+    } else {
+      setGraphCheckModalOpen(true);
+    }
+  };
+
+  const closeGraphCheckModal = () => {
+    setGraphCheckModalOpen(false);
+  };
+
+  return (
     <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
       <NavigationBar />
       <div style={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
-
         <Grid className={styles["main-grid"]} container spacing={2}>
-          <Grid item xs={4} sx={{height: '100%'}}>
+          <Grid item xs={4} sx={{ height: "100%" }}>
             <QuestionDescription
-              onSubmit={() => isComplete && handleModalOpen()}
+              onSubmit={handleSubmit}
               title={selectedQuestion.description.title}
             >
               <div>
@@ -46,20 +57,22 @@ const QuestionPage = () => {
                 </Typography>
                 <ul>
                   {selectedQuestion.description.plan.map((planItem, index) => (
-                    <li key={index}>
-                      {planItem}
-                    </li>
+                    <li key={index}>{planItem}</li>
                   ))}
                 </ul>
               </div>
             </QuestionDescription>
           </Grid>
-          <Grid item xs={8} sx={{height: '100%'}}>
+          <Grid item xs={8} sx={{ height: "100%" }}>
             <GraphApplication
-              initialGraph={selectedQuestion.initialGraph || { nodes: gitCommitPushNodes, edges: gitCommitPushEdges }}
-
-              goalGraph={selectedQuestion.goalGraph || { nodes: gitCommitPushNodesGoal, edges: gitCommitPushEdgesGoal }}
-              
+              initialGraph={{
+                nodes: gitCommitPushNodes,
+                edges: gitCommitPushEdges,
+              }}
+              goalGraph={{
+                nodes: gitCommitPushNodesGoal,
+                edges: gitCommitPushEdgesGoal,
+              }}
               answers={selectedQuestion.discussion.commands}
               setComplete={setComplete}
             />
@@ -67,9 +80,12 @@ const QuestionPage = () => {
         </Grid>
       </div>
       {isModalOpen && <QuestionEndModal onClose={handleModalClose} />}
+      <GraphCheckModal
+        isOpen={isGraphCheckModalOpen}
+        onClose={closeGraphCheckModal}
+      />
     </div>
   );
 };
-
 
 export default QuestionPage;
